6ce4bf7c6b92abcf7695ba63a4e3226179908ce8
Dunya F. Mammadli <dunyafm@code.local>
2021-12-20 03:17:19 -0800

    Practice

---
+++ Practice11/MyList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    public class MyList<T>
+    {
+        private T[] arr;
+        public int Count { get; set; }
+        public MyList() 
+        {
+            arr = new T[10];
+        }
+        public void Add(T obj)
+        {
+            if (Count % 10 == 0) 
+            {
+                Array.Resize(ref arr, arr.Length + 10);
+
+            }
+            arr[Count] = obj;
+            Count++;
+        }
+        public void Clear()
+        {
+          arr = new T[10];
+          Count = 0;
+        }
+        public void AddRange(IEnumerable<T> list)
+        {
+            foreach (var item in list)
+            {
+                Add(item);
+            }
+        }
+        public T Find(Predicate<T> func)
+        {
+            foreach (var item in arr)
+            {
+                if(func(item))
+                {
+                    return item;
+                }
+            }
+
+            return default;
+                            
+
+        }
+
+
+
+
+
+
+    }  
+  
+}
---
+++ Practice11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice
+{
+   public class Program
+   {
+        static void Main(string[] args)
+        {
+            MyList<int> nums = new MyList<int>();
+            List<int> newNums = new List<int>();
+            newNums.Add(88);
+            newNums.Add(89);
+            nums.AddRange(newNums);
+
+
+            nums.Add(1);
+            nums.Add(2);
+            nums.Add(3);
+            nums.Add(4);
+            nums.Add(5);
+            nums.Add(6);
+            nums.Add(7);
+            nums.Add(8);
+            nums.Add(9);
+            nums.Add(10);
+            nums.Add(11);
+            nums.Clear();
+
+            var result = nums.Find(m => m == 88);
+            Console.WriteLine(result);
+
+           
+
+
+
+
+        }
+   }
+}
